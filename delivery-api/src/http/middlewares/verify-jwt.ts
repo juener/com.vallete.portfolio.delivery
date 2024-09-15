8d01b29f5ae1794c118b6cc04d5fb87cfe3306ec
Juener <juener@gmail.com>
2024-09-15 04:57:45 +0100

    adjusting some logs

--- delivery-api/src/http/middlewares/verify-jwt.ts
+++ delivery-api/src/http/middlewares/verify-jwt.ts
@@ -1,9 +1,11 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function verifyJwt(request: FastifyRequest, reply: FastifyReply) {
   try {
-    await request.jwtVerify();
-    } catch (err) {
-    reply.status(401).send({ message: 'Unauthorized.' });
-    }
+    await request.jwtVerify()
+  } catch (err: any) {
+    reply
+      .status(401)
+      .send({ message: `Unauthorized. \n\n${err.message} \n\n${err}` })
+  }
 }
